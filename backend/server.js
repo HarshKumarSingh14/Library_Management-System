d7e9fbf138474a74de0c12a36eb88018acd7080c
HarshKumarSingh14 <hs324178@gmail.com>
2026-09-08 00:29:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["http://localhost:8080", "http://localhost:5173", "http://localhost:3000" "https://library-management-system-fdo2.onrender.com"], 
+    origin: ["http://localhost:8080", "http://localhost:5173", "http://localhost:3000", "https://library-management-system-fdo2.onrender.com"], 
     credentials: true 
 }));
 
