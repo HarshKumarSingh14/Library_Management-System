e2ed1803904262d1689af81497852abedc21d218
HarshKumarSingh14 <hs324178@gmail.com>
2026-09-08 19:23:29 +0530

    Update ForgotPasswordPage.tsx

--- frontend/src/pages/ForgotPasswordPage.tsx
+++ frontend/src/pages/ForgotPasswordPage.tsx
@@ -17,7 +17,7 @@ export default function ForgotPasswordPage() {
     
     try {
       // Asli Backend API Call
-      const response = await fetch('http://localhost:5000/api/auth/forgot-password', {
+      const response = await fetch('https://library-management-system-fdo2.onrender.com/api/auth/forgot-password', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
